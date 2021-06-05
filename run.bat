749ceb20b5c36af4dc0892199d424975813b151c
nguo <nerissaguo2018@gmail.com>
2021-06-05 19:46:34 -0400

    This is my 4th commit

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-pytest -s -v -m "sanity" --html=./Reports/marker.html testCasespackage/ --browser chrome 
+pytest -s -v -m "sanity" --html=./Reports/marker.html testCasespackage/ 
 rem pytest -s -v -m "sanity or regression" --html=./Reports/marker.html testCasespackage/ --browser firefox
 rem pytest -s -v -m "sanity and regression" --html=./Reports/marker.html testCasespackage/ --browser chrome
 rem pytest -s -v -m "regression" --html=./Reports/marker.html testCasespackage/ --browser chrome
