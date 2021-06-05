a85a076534f5a44f9923a7363fbac87b451fff0e
nguo <nerissaguo2018@gmail.com>
2021-06-05 14:01:34 -0400

    This is my 2nd commit after pull then commit

--- testCasespackage/createdingithub-2021-6-6.py
+++ testCasespackage/createdingithub-2021-6-6.py
@@ -1,2 +1,4 @@
 this is created directly from github under nopcommerceApp/testCasespackage/createdingithub-2021-6-6.py
 in order to test how to pull from git to local repo. 
+
+new added from local file. 
